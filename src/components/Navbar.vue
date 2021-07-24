fa49a5356230e5823bfb4314bcf7dd7295f0bdfb
“trangluu” <tranglp.1995@gmail.com>
2021-07-24 17:05:36 +0900

    Navbar Component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there... display name here</p>
+      <p class="email">Currently logged in as... email</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email {
+  font-size: 14px;
+  color: #999;
+}
+</style>
