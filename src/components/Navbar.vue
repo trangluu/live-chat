1b7f8ad691a7a207770005d9a36f311525165f75
“trangluu” <tranglp.1995@gmail.com>
2021-07-24 17:32:37 +0900

    ログアウト処理

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,12 +4,28 @@
       <p>Hey there... display name here</p>
       <p class="email">Currently logged in as... email</p>
     </div>
-    <button>Logout</button>
+    <button @click="handleLogout">Logout</button>
   </nav>
 </template>
 
 <script>
+import useLogout from '../composables/useLogout'
+export default {
+  setup() {
+    const { logout, error } = useLogout()
 
+    const handleLogout = async () => {
+      await logout()
+      if (!error.value) {
+        console.log('user logged out')
+      }
+    }
+
+    return {
+      handleLogout
+    }
+  }
+}
 </script>
 
 <style scoped>
